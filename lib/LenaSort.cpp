2c0d303de358285f1c01feb91eba3d8b603b997b
Richard Longland <rllongla@ncsu.edu>
2025-05-07 11:21:37 -0400

    Clean up some terminal output from LenaSort

--- lib/LenaSort.cpp
+++ lib/LenaSort.cpp
@@ -936,8 +936,8 @@ void MidasAnalyzerRun::BeginRun(TARunInfo *runinfo)
 	uint32_t run_start_time_binary = 0;
 	//runinfo->fOdb->RU32("/Runinfo/Start time binary", &run_start_time_binary);
 	time_t run_start_time = run_start_time_binary;
-	printf("ODB Run start time: %d: %s", (int)run_start_time,
-	       ctime(&run_start_time));
+	//printf("ODB Run start time: %d: %s", (int)run_start_time,
+	//       ctime(&run_start_time));
 
 	fRunEventCounter = 0;
 }
